1464e31876c356ffe53f72e783df673362dbe9c1
Gopika <20088592@mydbs.ie>
2026-02-26 23:36:35 +0000

    Fix Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,12 +36,12 @@ resource "aws_security_group" "ssh_access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-   
+
   ingress {
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
